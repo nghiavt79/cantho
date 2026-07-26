fd88d3861f1f3a19b55aba4fbb32177a098dad6e
nghiavt79 <nghiavt@gmail.com>
2026-07-26 18:14:29 +0700

    Fix SysFunction route seed script

--- TechExchangeApp/Scripts/update-sysfunction-mvc-routes.sql
+++ TechExchangeApp/Scripts/update-sysfunction-mvc-routes.sql
@@ -1,9 +1,24 @@
 SET NOCOUNT ON;
+SET XACT_ABORT ON;
 
 BEGIN TRAN;
 
-MERGE dbo.SysFunction AS target
-USING (VALUES
+DECLARE @Menu TABLE
+(
+    FunctionId int NOT NULL PRIMARY KEY,
+    FunctionName nvarchar(500) NOT NULL,
+    URL nvarchar(300) NULL,
+    HrefName nvarchar(100) NULL,
+    IsMenu bit NOT NULL,
+    IsStatus bit NOT NULL,
+    IsShow bit NOT NULL,
+    ParentId int NULL,
+    Sort int NULL,
+    SiteId int NULL
+);
+
+INSERT INTO @Menu (FunctionId, FunctionName, URL, HrefName, IsMenu, IsStatus, IsShow, ParentId, Sort, SiteId)
+VALUES
     (569, N'Dashboard', N'cms/Dashboard', N'Dashboard', 1, 1, 1, 0, 10, 1),
     (16, N'Users', N'cms/Users', N'Người dùng', 1, 1, 1, 0, 20, 1),
     (570, N'Projects', N'cms/ProjectsAdmin', N'Dự án', 1, 1, 1, 0, 30, 1),
@@ -20,34 +35,34 @@ USING (VALUES
     (422, N'PhieuYeuCauCNTB', N'cms/PhieuYeuCauCNTBAdmin', N'Phiếu yêu cầu', 1, 1, 1, 0, 140, 1),
     (574, N'ContentsYeuCauAdmin', N'cms/ContentsYeuCauAdmin', N'Tìm mua công khai', 1, 1, 1, 0, 150, 1),
     (41, N'Feedback', N'cms/FeedbackAdmin', N'Phản hồi (Feedback)', 1, 1, 1, 0, 160, 1),
+    (555, N'Feedback', N'cms/FeedbackAdmin', N'Phản hồi (Feedback)', 1, 1, 1, 0, 161, 1),
     (575, N'DashboardData', N'cms/DashboardData', N'Số liệu Dashboard', 1, 1, 1, 0, 170, 1),
     (556, N'SysParam', N'cms/SystemParameterAdmin', N'Tham số hệ thống', 1, 1, 1, 0, 180, 1),
-    (51, N'SysFunction', N'cms/SysFunctionAdmin', N'SysFunction', 1, 1, 1, 0, 190, 1)
-) AS source (FunctionId, FunctionName, URL, HrefName, IsMenu, IsStatus, IsShow, ParentId, Sort, SiteId)
-ON target.FunctionId = source.FunctionId
-WHEN MATCHED THEN
-    UPDATE SET
-        target.FunctionName = source.FunctionName,
-        target.URL = source.URL,
-        target.HrefName = source.HrefName,
-        target.IsMenu = source.IsMenu,
-        target.IsStatus = source.IsStatus,
-        target.IsShow = source.IsShow,
-        target.ParentId = source.ParentId,
-        target.Sort = source.Sort,
-        target.SiteId = source.SiteId
-WHEN NOT MATCHED BY TARGET THEN
-    INSERT (FunctionId, FunctionName, URL, HrefName, IsMenu, IsStatus, IsShow, ParentId, Sort, SiteId)
-    VALUES (source.FunctionId, source.FunctionName, source.URL, source.HrefName, source.IsMenu, source.IsStatus, source.IsShow, source.ParentId, source.Sort, source.SiteId);
+    (51, N'SysFunction', N'cms/SysFunctionAdmin', N'SysFunction', 1, 1, 1, 0, 190, 1);
 
-UPDATE dbo.SysFunction
-SET URL = N'cms/FeedbackAdmin',
-    FunctionName = N'Feedback',
-    HrefName = N'Phản hồi (Feedback)',
-    IsMenu = 1,
-    IsStatus = 1,
-    IsShow = 1
-WHERE FunctionId = 555;
+UPDATE target
+SET target.FunctionName = source.FunctionName,
+    target.URL = source.URL,
+    target.HrefName = source.HrefName,
+    target.IsMenu = source.IsMenu,
+    target.IsStatus = source.IsStatus,
+    target.IsShow = source.IsShow,
+    target.ParentId = source.ParentId,
+    target.Sort = source.Sort,
+    target.SiteId = source.SiteId
+FROM dbo.SysFunction AS target
+INNER JOIN @Menu AS source ON source.FunctionId = target.FunctionId;
+
+INSERT INTO dbo.SysFunction (FunctionId, FunctionName, URL, HrefName, IsMenu, IsStatus, IsShow, ParentId, Sort, SiteId)
+SELECT source.FunctionId, source.FunctionName, source.URL, source.HrefName, source.IsMenu, source.IsStatus,
+       source.IsShow, source.ParentId, source.Sort, source.SiteId
+FROM @Menu AS source
+WHERE NOT EXISTS
+(
+    SELECT 1
+    FROM dbo.SysFunction AS target
+    WHERE target.FunctionId = source.FunctionId
+);
 
 COMMIT;
 
